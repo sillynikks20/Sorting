88415f66456096b35834566c9f71fce48553e21e
HP <HP@10.12.25.176>
2022-09-09 17:17:01 +0530

    new sort

--- src/NewSort.java
+++ src/NewSort.java
@@ -15,7 +15,7 @@ public class NewSort {
             arr[i] = smallerNumber;  
         }  
 		for(int i=0; i < arr.length; i++) {
-        	System.out.print(arr[i]+ " ");
+        	System.out.print(arr[i]+ "  ");
         }
 	}
 
