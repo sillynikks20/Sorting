8eba0957b0b8fc1adb78bcd3f80e330495bf7864
HP <HP@10.12.25.176>
2022-09-09 16:28:52 +0530

    Bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BubbleSort 
+{
+
+	public static void main(String[] args) 
+	{
+		int num;
+	    System.out.println("Enter an  Integer number:");
+
+	 
+	    Scanner input = new Scanner(System.in);
+	    num = input.nextInt();
+
+	   
+	    if ( num % 2 == 0 )
+	        System.out.println("Entered number is even");
+	     else
+	        System.out.println("Entered number is odd");
+	
+
+	}
+
+}
